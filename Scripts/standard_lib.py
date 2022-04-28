914693417a816cdcb3171c08a7a62fa6086a4a2b
Luca Quartesan <quarte1994@gmail.com>
2022-04-28 17:42:22 +0200

    saving a json

--- Scripts/standard_lib.py
+++ Scripts/standard_lib.py
@@ -1,6 +1,17 @@
 import unreal
 import datetime
 import random
+import json
 
 unreal.log("It's {}".format(datetime.datetime.now()))
 unreal.log("A dice roll: {}".format(random.randrange(1, 6)))
+
+user_config = {
+  "name": "Bob",
+  "screen_size": (1920, 1080),
+  "is_active": True,   
+}
+
+unreal.log("The project is located at {}".format(unreal.Paths.project_dir()))
+with open(unreal.Paths.project_dir()+'user_config.json', 'w') as f:
+    json.dump(user_config, f)
