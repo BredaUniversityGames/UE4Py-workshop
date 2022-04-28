9f4ebddcbbfbb62c675fbd4f4b80c3ef10de50a9
Luca Quartesan <quarte1994@gmail.com>
2022-04-28 18:53:02 +0200

    better greeting message

--- Scripts/startup.py
+++ Scripts/startup.py
@@ -1,7 +1,19 @@
 import unreal 
 import json
+import datetime
 
 config_path = unreal.Paths.project_dir() + 'user_config.json'
 with open(config_path) as json_file:
     user_config = json.load(json_file)
-unreal.log("Hi {}!".format(user_config["name"]))
+
+now = datetime.datetime.now()
+message = ["Good"]
+if now.hour < 12:
+    message.append("morning")
+elif now.hour < 18:
+    message.append("afternoon")
+else:
+    message.append("evening")
+
+message.append(user_config["name"])
+unreal.log(" ".join(message))
