c099352232cd2e0aa24fc30c391e8f71382c90e6
Luca Quartesan <quarte1994@gmail.com>
2022-06-03 12:03:00 +0200

    BP library for greetings

--- Scripts/startup.py
+++ Scripts/startup.py
@@ -6,14 +6,35 @@ config_path = unreal.Paths.project_dir() + 'user_config.json'
 with open(config_path) as json_file:
     user_config = json.load(json_file)
 
-now = datetime.datetime.now()
-message = ["Good"]
-if now.hour < 12:
-    message.append("morning")
-elif now.hour < 18:
-    message.append("afternoon")
-else:
-    message.append("evening")
+@unreal.uclass()
+class WelcomeLibrary(unreal.BlueprintFunctionLibrary):
+    # pure function = no exec pin
+    @unreal.ufunction(ret=str, static=True, pure=True, meta=dict(Category="Welcome Python Lib"))
+    def get_part_of_day():
+        now = datetime.datetime.now()
+        if now.hour < 12:
+            return "morning"
+        elif now.hour < 18:
+            return "afternoon"
+        else:
+            return "evening"
 
-message.append(user_config["name"])
-unreal.log(" ".join(message))
+    @unreal.ufunction(ret=str, static=True, pure=True, meta=dict(Category="Welcome Python Lib"))
+    def get_user_name():
+        return user_config["name"]
+    
+    @unreal.ufunction(ret=str, static=True, pure=True, meta=dict(Category="Welcome Python Lib"))
+    def get_welcome_message():
+        message = [
+            "Good",
+            WelcomeLibrary.get_part_of_day(),
+            WelcomeLibrary.get_user_name(),
+            ]
+        
+        return " ".join(message)
+
+    @unreal.ufunction(static=True, pure=True, meta=dict(Category="Welcome Python Lib"))
+    def log_welcome_message():
+        unreal.log(WelcomeLibrary.get_welcome_message())
+
+WelcomeLibrary.log_welcome_message()
