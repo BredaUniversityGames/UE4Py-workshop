82bd6fd3439201b60d874af482a5febd04af8592
Luca Quartesan <quarte1994@gmail.com>
2022-06-03 15:04:03 +0200

    moved/rename welcome widget

--- Scripts/editor_utils.py
+++ Scripts/editor_utils.py
@@ -31,6 +31,22 @@ class EditorUtilsLib(unreal.BlueprintFunctionLibrary):
                 prefix = "MI"
             target_path_name = "/".join(("/Game", dir, "_".join((prefix, asset.get_name()))))
             unreal.EditorAssetLibrary.rename_asset(asset.get_path_name(), target_path_name)
-
+    
+    @unreal.ufunction(params=[int], static=True, meta=dict(Category="Editor Utils lib"))
+    def create_blueprint(count):
+        for i in range(count):
+            # if we try to create an asset at a certain folder that doesn't exists
+            # unreal will automatically catch that and create the folder structure
+            bp_name = "MyPlayerController{}".format(i) 
+            bp_path = "/Game/PlayerController"
+            # assets are created through factories 
+            factory = unreal.BlueprintFactory()
+            # we need to set the parent in the factory 
+            # note that also factory derives from object
+            factory.set_editor_property("ParentClass", unreal.PlayerController)
+            asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
+            asset = asset_tools.create_asset(bp_name, bp_path, None, factory)
+            # much faster to change than in c++ no need to recompile anything :)
+            unreal.EditorAssetLibrary.save_loaded_asset(asset)
 
 unreal.log("Imported PyEditorUtils")
